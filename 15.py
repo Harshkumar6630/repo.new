5cb193447140fab74cf103ea40c1229efa43b919
Harshkumar6630 <152527459+Harshkumar6630@users.noreply.github.com>
2023-12-01 09:38:29 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+print("{} + {} = {}".format(num1, num2, num1+num2))
+print("{} - {} = {}".format(num1, num2, num1-num2))
+print("{} * {} = {}".format(num1, num2, num1*num2))
+print("{} / {} = {}".format(num1, num2, num1/num2))
+print("{} ** {} = {}".format(num1, num2, num1**num2))
+print("{} % {} = {}".format(num1, num2, num1%num2))
+print("{} // {} = {}".format(num1, num2, num1//num2))
+
+a=45
+b=98
+c=divmod(a,b)
+print(c)
